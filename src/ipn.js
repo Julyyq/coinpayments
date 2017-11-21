df0e4bba4fef61b07cdd6c3b38d8de57a7de951a
Julyyq <colorfuljuly@gmail.com>
2017-11-21 12:23:40 +0800

    fix for eggjs

--- src/ipn.js
+++ src/ipn.js
@@ -1,49 +1,55 @@
-const
-	qs     = require('querystring'),
-	crypto = require('crypto');
+'use strict';
 
-module.exports = (function () {
+var qs = require('querystring'),
+    crypto = require('crypto');
 
-	function IPN ({merchantId, merchantSecret}) {
-		if(!merchantId || !merchantSecret) {
+module.exports = function () {
+
+  function IPN(_ref,ctx,next) {
+    var _this = this;
+
+    var merchantId = _ref.merchantId,
+        merchantSecret = _ref.merchantSecret;
+
+    if (!merchantId || !merchantSecret) {
       throw "Merchant ID and Merchant Secret are needed";
     }
-    let hmac;
+    var hmac = void 0;
 
-    let getPrivateHeadersIPN = function (parameters) {
-			let signature, paramString;
+    var getPrivateHeadersIPN = function getPrivateHeadersIPN(parameters) {
+      var signature = void 0,
+          paramString = void 0;
 
-			paramString = qs.stringify(parameters).replace(/%20/g, '+')
+      paramString = qs.stringify(parameters).replace(/%20/g, '+');
 
-	  	signature = crypto.createHmac('sha512', merchantSecret).update(paramString).digest('hex');
+      signature = crypto.createHmac('sha512', merchantSecret).update(paramString).digest('hex');
 
-	  	return signature;
-		}
+      return signature;
+    };
 
-    return (req,res,next) => {
-      if(!req.get('HMAC') || !req.body || !req.body.ipn_mode || req.body.ipn_mode != 'hmac' || merchantId != req.body.merchant) {
-        return next("COINPAYMENTS_INVALID_REQUEST");
-      }
-      hmac = getPrivateHeadersIPN(req.body);
-      if(hmac != req.get('HMAC')) {
-        return next("COINPAYMENTS_INVALID_REQUEST");
-      }
-      res.end();
-      if(req.body.status < 0) {
-        this.emit('ipn_fail', req.body);
-        return next();
-      }
-      if(req.body.status < 100){
-        this.emit('ipn_pending', req.body);
-        return next();
-      }
-      if(req.body.status == 100) {
-        this.emit('ipn_complete', req.body);
-        return next();
-      }
+    const req = ctx.request;
+    const res = ctx.response;
+    if (!req.get('HMAC') || !req.body || !req.body.ipn_mode || req.body.ipn_mode != 'hmac' || merchantId != req.body.merchant) {
+      return next("COINPAYMENTS_INVALID_REQUEST");
     }
-	}
-	
-	return IPN;
+    hmac = getPrivateHeadersIPN(req.body);
+    if (hmac != req.get('HMAC')) {
+      return next("COINPAYMENTS_INVALID_REQUEST");
+    }
+    res.end();
+    if (req.body.status < 0) {
+      _this.emit('ipn_fail', req.body);
+      return next();
+    }
+    if (req.body.status < 100) {
+      _this.emit('ipn_pending', req.body);
+      return next();
+    }
+    if (req.body.status == 100) {
+      _this.emit('ipn_complete', req.body);
+      return next();
+    }
+  }
 
-})();
+  return IPN;
+}();
